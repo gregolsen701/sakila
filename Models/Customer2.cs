fc72fdd67ddd5fbfe31ee59cfd9bebecab906829
Greg Olsen <gregolsen701@gmail.com>
2020-09-16 13:03:21 -0500

    Add project files.

---
+++ Models/Customer2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Sakila1.Models
+{
+    public partial class Customer2
+    {
+        public int CustomerId { get; set; }
+        public int StoreId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int AddressId { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public byte[] Crcard { get; set; }
+    }
+}
